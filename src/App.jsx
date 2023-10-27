13e0c57c0eaccb0b1e1d72592ea1cf4fe4871b0d
sushaz9 <susannahelisabeth1@gmail.com>
2023-10-27 15:14:19 +0100

    twopages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>Susie Harrison</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <footer>
+        <p>&copy; SEH</p>
+      </footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
