db8535495c8da21346d7708e0275341b7f6c30d1
sushaz9 <susannahelisabeth1@gmail.com>
2023-10-27 16:17:24 +0100

    sillycss

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,24 @@ export default function About() {
     <main>
       <h2>HI!</h2>
       <p>I'm Susie. Here you can read all about me!</p>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <h1>My hobbies</h1>
+        </header>
+        <main>
+          <section>
+            <h2>My job </h2>
+            <p>This is the first section of your content.</p>
+          </section>
+          <section>
+            <h2>My family</h2>
+            <p>This is the second section of your content.</p>
+          </section>
+        </main>
+        <footer>
+          <p>&copy; SEH</p>
+        </footer>
+      </div>
     </main>
   );
 }
